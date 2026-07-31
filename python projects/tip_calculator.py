7b582b6e8c7962155b34d9251aef57f6a2bdde1d
sudekahya <sudekahya353@icloud.com>
2026-07-31 22:22:56 +0000

    python projects

---
+++ python projects/tip_calculator.py
@@ -0,0 +1,13 @@
+print("Hello, welcome to the tip calculator")
+
+bill = float(input("What was the total bill? $"))
+tip = int(input("How much tip would you like to give? 10, 12, or 15? "))
+people = int(input("How many people to split the bill? "))
+
+bill_with_tip = tip / 100 * bill + bill
+print(bill_with_tip)
+
+bill_per_person = bill_with_tip / people
+
+final_amount = round(bill_per_person, 2)
+print(f"Each person should pay: ${final_amount}")
---
+++ python projects/treasure_island.py
@@ -0,0 +1,20 @@
+print("welcome to the treasure island!")
+print("your mission is to find the treasure.")
+
+step_1 = input("you're at a cross road.where do you want to go? type left or right ")
+if step_1 == "left":
+    print("you come to a lake there is an island in the middle of the lake.")
+    step_2 = input("type wait to wait for a boat.type swim to swim across.")
+    if step_2 == "wait":
+        print("you arrive at the island unharmed. there is a house with 3 doors.")
+        step_3 = input("one red, one yellow and one blue which colour do you choose?")
+        if step_3 == "red":
+            print("it's a room full of fire game over")
+        elif step_3 == "yellow":
+            print("you found the treasure!you win!")
+        elif step_3 == "blue":
+            print("you enter a room of beasts game over")
+    else:
+        print("you got attacked by an angry trout game over")
+else:
+    print("you fell into a hole game over")
